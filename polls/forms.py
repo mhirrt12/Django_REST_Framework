2ef5e02c4dcf192ed465f626d1f44498c4302520
mhirrt12 <kaletamene90@gmail.com>
2026-08-03 22:13:00 -0700

    Add BookForm for managing book entries in polls app

--- polls/forms.py
+++ polls/forms.py
@@ -1,6 +1,8 @@
 from django import forms
+from .models import Book
 
-class ContactForm(forms.Form):
-    name = forms.CharField()
-    email = forms.EmailField()
-    message = forms.CharField(widget=forms.Textarea)
+class BookForm(forms.ModelForm):
+
+    class Meta:
+        model = Book
+        fields = ["title", "price"]

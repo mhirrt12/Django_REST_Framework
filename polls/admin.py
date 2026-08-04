4c90ac308ce861d4ae71d6820f0102327879c352
mhirrt12 <kaletamene90@gmail.com>
2026-08-03 22:28:28 -0700

    Add BookAdmin class to manage Book model in admin interface

--- polls/admin.py
+++ polls/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Student
+from .models import Student,Book, Product
 
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
     list_display = ('name', 'age', 'email')
     search_fields = ('name', 'email')
     list_filter = ('age',)
+    
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price')
+    # search_fields = ('title',)
+    # list_filter = ('price',)

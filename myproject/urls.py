d9d0174854ac4607519147f5428f461b0530263c
mhirrt12 <kaletamene90@gmail.com>
2026-08-03 22:20:14 -0700

    Add URL pattern for book form submission in urlpatterns

--- myproject/urls.py
+++ myproject/urls.py
@@ -21,5 +21,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", views.home),
+    path("form/",views.add_book),
     # path("polls/", include("polls.urls")),
 ]

6ae85653b6e07ef7d6b27784439f0d1445ea68bf
mhirrt12 <kaletamene90@gmail.com>
2026-08-03 22:48:05 -0700

    Add blank line after Book model for improved readability

--- polls/models.py
+++ polls/models.py
@@ -14,6 +14,7 @@ class Book(models.Model):
     title = models.CharField(max_length=100)
     price = models.FloatField()
     
+    
 class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)

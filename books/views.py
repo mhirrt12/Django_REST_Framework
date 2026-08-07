eb5432282f0793e51a91e8ea2ace11151318e45d
mhirrt12 <kaletamene90@gmail.com>
2026-08-07 10:47:21 -0700

    Move model and serializer imports to the top of views.py for better organization

--- books/views.py
+++ books/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 from rest_framework.views import APIView
 from rest_framework.response import Response
+
+from .models import Book
+from .serializers import BookSerializer
 class BookList(APIView):
 
     def get(self, request):

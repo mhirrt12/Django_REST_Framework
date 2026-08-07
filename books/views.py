67c3fe0b9f9d516d96c960f2a2c036c233157310
mhirrt12 <kaletamene90@gmail.com>
2026-08-07 10:41:38 -0700

    Implement BookList API view to return a greeting message

--- books/views.py
+++ books/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 from rest_framework.views import APIView
 from rest_framework.response import Response
+class BookList(APIView):
+
+    def get(self, request):
+        return Response({
+            "message": "Hello from Book API"
+        })
